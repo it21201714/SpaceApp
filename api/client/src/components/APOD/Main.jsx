9f706f13feb35a35e98ed0ab7aaaf7839d6e86d3
it21201714 <it21201714@my.sliit.lk>
2024-05-08 17:30:56 +0530

    Api Changed

--- api/client/src/components/APOD/Main.jsx
+++ api/client/src/components/APOD/Main.jsx
@@ -1,9 +1,27 @@
 export default function Main(props) {
-
   const { data } = props;
+
+  // Check if a video link is provided in the data
+  const isVideo = data && data.media_type === "video";
+
   return (
     <div className="imgContainer">
-      <img src={data.hdurl} alt={data.title} className="bgImage" />
+      {isVideo ? (
+        // If it's a video, render an iframe for embedding YouTube video
+        <iframe
+          className="w-full h-full"
+          title="YouTube Video"
+          width="100%"
+          height="auto"
+          src={data.url}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      ) : (
+        // If it's not a video, render an image
+        <img src={data.hdurl} alt={data.title} className="bgImage" />
+      )}
     </div>
   );
 }
