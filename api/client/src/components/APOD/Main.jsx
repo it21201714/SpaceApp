aa26bc794b44d4a266f244897266816ffc59da1d
it21201714 <it21201714@my.sliit.lk>
2024-05-08 17:31:27 +0530

    Changes Made

--- api/client/src/components/APOD/Main.jsx
+++ api/client/src/components/APOD/Main.jsx
@@ -1,13 +1,11 @@
 export default function Main(props) {
   const { data } = props;
 
-  // Check if a video link is provided in the data
   const isVideo = data && data.media_type === "video";
 
   return (
     <div className="imgContainer">
       {isVideo ? (
-        // If it's a video, render an iframe for embedding YouTube video
         <iframe
           className="w-full h-full"
           title="YouTube Video"
@@ -19,7 +17,7 @@ export default function Main(props) {
           allowFullScreen
         ></iframe>
       ) : (
-        // If it's not a video, render an image
+       
         <img src={data.hdurl} alt={data.title} className="bgImage" />
       )}
     </div>
